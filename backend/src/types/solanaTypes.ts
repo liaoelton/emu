eb3b755e63f13ef61ecec595c966fed954b6325b
Elton Liao <41299809+liaoelton@users.noreply.github.com>
2024-07-29 00:03:04 +0800

    feat: Add API to get slot and blocks (#2)
    - `/slot`: To fetch latest slot
    - `/blocks?end=`: To retrieve recent (5) blocks by slot for frontend
    display

--- backend/src/types/solanaTypes.ts
+++ backend/src/types/solanaTypes.ts
@@ -2,5 +2,4 @@ import { VersionedBlockResponse } from "@solana/web3.js";
 
 export interface ExtendedBlockResponse extends VersionedBlockResponse {
     blockHeight: number;
-    blockTime: number;
 }
