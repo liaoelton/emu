dc18e9c8887444af335ba7172ca4c5a0bfcdec85
Elton <Liao.elton@gmail.com>
2024-07-29 00:02:20 +0800

    fix

--- backend/src/types/solanaTypes.ts
+++ backend/src/types/solanaTypes.ts
@@ -1,4 +1,4 @@
-import { VersionedBlockResponse, VersionedTransactionResponse } from "@solana/web3.js";
+import { VersionedBlockResponse } from "@solana/web3.js";
 
 export interface ExtendedBlockResponse extends VersionedBlockResponse {
     blockHeight: number;
