1a468e11b2dcca111d06bfff66eaee9796af5efa
Elton Liao <41299809+liaoelton@users.noreply.github.com>
2024-07-29 17:42:02 +0800

    feat: Add search API and frontend functionality (#7)
    - add search API for determine block/tx/address type
    - add search and redirect functionality into the frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import { Connection } from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
@@ -85,6 +85,51 @@ app.get("/tx/:signature", async (req, res) => {
         console.error("Failed to fetch or save transaction info:", error);
         res.status(500).json({ error: error.toString() });
     }
+});
+
+app.get("/search/:query", async (req, res) => {
+    const { query } = req.params;
+    const slot = parseInt(query, 10);
+
+    if (/^\d+$/.test(query)) {
+        try {
+            // TODO: Handle uncomfirmed blocks
+            const blockInfo = await findOrCreateBlockBySlot(connection, slot);
+            res.json({ type: "block", data: blockInfo });
+        } catch (error: any) {
+            console.error("Error fetching block:", error);
+            res.status(500).json({ error: error.message });
+        }
+        return;
+    }
+    // Check if it's base58 encoded (signature, account, or program)
+    if (/^[1-9A-HJ-NP-Za-km-z]+$/.test(query)) {
+        try {
+            const transaction = await findOrCreateTransactionBySignature(connection, query);
+            if (transaction) {
+                res.json({ type: "transaction", data: transaction });
+                return;
+            }
+        } catch (error) {
+            console.error("Error fetching transaction:", error);
+        }
+
+        try {
+            // TODO: Handle unconstructed accounts
+            const addressInfo = await connection.getAccountInfo(new PublicKey(query));
+            if (addressInfo) {
+                res.json({ type: "address", data: addressInfo });
+                return;
+            }
+        } catch (error) {
+            console.error("Error fetching address info:", error);
+        }
+
+        res.status(404).json({ type: "error", error: "Block, transaction, or address not found" });
+        return;
+    }
+
+    res.status(400).json({ error: "Query format not recognized as a slot or base58 encoded string" });
 });
 
 connect_to_db().then(() =>
