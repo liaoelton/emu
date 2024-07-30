00724606503db25926896e2b5d162f31d664e77f
Elton <Liao.elton@gmail.com>
2024-07-30 18:56:26 +0800

    refactor

--- frontend/src/pages/tx/[signature].tsx
+++ frontend/src/pages/tx/[signature].tsx
@@ -2,7 +2,7 @@ import TxInfo from "@/components/tx/TxInfo";
 import TxInstructions from "@/components/tx/TxInstructions";
 import TxLogs from "@/components/tx/TxLogs";
 import { Transaction } from "@/types/Transaction";
-import { parseTransaction } from "@/utils/parsing";
+import { getTxInstructions } from "@/utils/txHelpers";
 import { Flex, Grid, Text } from "@mantine/core";
 import axios from "axios";
 import { useRouter } from "next/router";
@@ -18,85 +18,15 @@ const Tx = () => {
     const [programs, setPrograms] = useState<any>({});
 
     useEffect(() => {
-        const fetchDecodedData = async () => {
-            if (!transaction) return;
-            try {
-                const accountKeys = transaction.transaction.message.accountKeys;
-                let ixIndex = 0;
-                let ixs: any[] = [];
-                let programs = {};
-                for (const instruction of transaction.transaction.message.instructions) {
-                    const parsedResult = await parseTransaction(
-                        accountKeys[instruction.programIdIndex],
-                        instruction.data
-                    );
-                    let SFMIdlItem = null;
-                    let decodedData = null;
-                    if (parsedResult) {
-                        SFMIdlItem = parsedResult.SFMIdlItem;
-                        decodedData = parsedResult.decodedData;
-                    }
-                    programs = {
-                        ...programs,
-                        [SFMIdlItem?.programId as string]: SFMIdlItem?.idl.name as string
-                    };
-                    decodedData = {
-                        ...decodedData,
-                        index: ixIndex,
-                        accounts: instruction.accounts.map((accountIndex: number) => accountKeys[accountIndex]),
-                        data: decodedData?.data,
-                        name: decodedData?.name,
-                        idl_name: SFMIdlItem?.idl.name,
-                        idl_program_id: SFMIdlItem?.programId,
-                    } as any;
-
-                    if (transaction.meta.innerInstructions.map((inner: any) => inner.index).includes(ixIndex)) {
-                        let inner_ixs: any[] = [];
-                        for (const inner of transaction.meta.innerInstructions) {
-                            for (const inner_ix of inner.instructions) {
-                                const innerParsedResult = await parseTransaction(
-                                    accountKeys[inner_ix.programIdIndex],
-                                    inner_ix.data
-                                );
-                                let innerDecodedData = null;
-                                let innerSFMIdlItem = null;
-                                if (innerParsedResult) {
-                                    innerDecodedData = innerParsedResult.decodedData;
-                                    innerSFMIdlItem = innerParsedResult.SFMIdlItem;
-                                    inner_ixs.push({
-                                        ...innerDecodedData,
-                                        idl_name: innerSFMIdlItem?.idl.name,
-                                        idl_program_id: innerSFMIdlItem?.programId,
-                                        accounts: inner_ix?.accounts.map(
-                                            (accountIndex: number) => accountKeys[accountIndex]
-                                        ),
-                                        data: innerDecodedData?.data,
-                                        name: innerDecodedData?.name,
-                                        innerSFMIdlItem,
-                                    });
-                                    programs = {
-                                        ...programs,
-                                        [innerSFMIdlItem?.programId as string]: innerSFMIdlItem?.idl.name as string,
-                                    };
-                                }
-                            }
-                        }
-                        decodedData = {
-                            ...decodedData,
-                            inner: inner_ixs,
-                        } as any;
-                    }
-                    ixs.push(decodedData);
-                    ixIndex++;
-                }
+        const updateTransactionData = async () => {
+            if (transaction) {
+                const { ixs, programs } = await getTxInstructions(transaction);
                 setIxs(ixs);
                 setPrograms(programs);
-            } catch (error) {
-                console.error("Failed to parse transaction:", error);
             }
         };
 
-        fetchDecodedData();
+        updateTransactionData();
     }, [transaction]);
 
     useEffect(() => {
@@ -133,17 +63,15 @@ const Tx = () => {
 
     return (
         <Flex w="100%" direction="column" gap="md" mb="xl">
-            <Flex align="baseline" gap="md">
-                <Text size="xl" fw={700} mb="md">
-                    Transaction Details
-                </Text>
-            </Flex>
+            <Text size="xl" fw={700} mb="md">
+                Transaction Details
+            </Text>
             <Grid className="dashboard-cards">
                 {transaction && (
                     <>
                         <TxInfo signature={signature} transaction={transaction} />
                         <TxInstructions ixs={ixs} />
-                        <TxLogs logs={transaction.meta.logMessages} programs={programs}/>
+                        <TxLogs logs={transaction.meta.logMessages} programs={programs} />
                     </>
                 )}
             </Grid>
---
+++ frontend/src/utils/txHelpers.ts
@@ -0,0 +1,87 @@
+import { Transaction } from "@/types/Transaction";
+import { checkIfInstructionParser, ParserType, SolanaFMParser } from "@solanafm/explorer-kit";
+import { getProgramIdl } from "@solanafm/explorer-kit-idls";
+
+export async function parseTransaction(programId: string, ixData: string) {
+    try {
+        const SFMIdlItem = await getProgramIdl(programId);
+        if (!SFMIdlItem) {
+            throw new Error(`No IDL found for program ID: ${programId}`);
+        }
+
+        const parser = new SolanaFMParser(SFMIdlItem, programId);
+        const instructionParser = parser.createParser(ParserType.INSTRUCTION);
+
+        if (!instructionParser || !checkIfInstructionParser(instructionParser)) {
+            throw new Error(`Failed to create a valid instruction parser for program ID: ${programId}`);
+        }
+
+        const decodedData = instructionParser.parseInstructions(ixData);
+        return { SFMIdlItem, decodedData };
+    } catch (error) {
+        console.error(`Error parsing transaction: ${(error as Error).message}`);
+        return { SFMIdlItem: null, decodedData: null };
+    }
+}
+
+export const getTxInstructions = async (
+    transaction: Transaction | null
+): Promise<{ ixs: any[]; programs: Record<string, string> }> => {
+    if (!transaction) {
+        return { ixs: [], programs: {} };
+    }
+    const accountKeys = transaction.transaction.message.accountKeys;
+    const instructions = transaction.transaction.message.instructions;
+    const innerInstructions = transaction.meta.innerInstructions;
+    const programs: Record<string, string> = {};
+    const ixs = await Promise.all(
+        instructions.map(async (instruction, ixIndex) => {
+            const parsedResult = await parseTransaction(accountKeys[instruction.programIdIndex], instruction.data);
+            const SFMIdlItem = parsedResult?.SFMIdlItem || null;
+            const decodedData = parsedResult?.decodedData || null;
+
+            if (SFMIdlItem) {
+                programs[SFMIdlItem.programId] = SFMIdlItem.idl.name;
+            }
+
+            const instructionData = {
+                ...decodedData,
+                index: ixIndex,
+                accounts: instruction.accounts.map((accountIndex: number) => accountKeys[accountIndex]),
+                idl_name: SFMIdlItem?.idl.name,
+                idl_program_id: SFMIdlItem?.programId,
+            };
+
+            if (innerInstructions.some((inner: any) => inner.index === ixIndex)) {
+                const inner_ixs = await Promise.all(
+                    innerInstructions.flatMap((inner: any) =>
+                        inner.instructions.map(async (inner_ix: any) => {
+                            const innerParsedResult = await parseTransaction(
+                                accountKeys[inner_ix.programIdIndex],
+                                inner_ix.data
+                            );
+                            const innerSFMIdlItem = innerParsedResult?.SFMIdlItem || null;
+                            const innerDecodedData = innerParsedResult?.decodedData || null;
+
+                            if (innerSFMIdlItem) {
+                                programs[innerSFMIdlItem.programId] = innerSFMIdlItem.idl.name;
+                            }
+
+                            return {
+                                ...innerDecodedData,
+                                idl_name: innerSFMIdlItem?.idl.name,
+                                idl_program_id: innerSFMIdlItem?.programId,
+                                accounts: inner_ix.accounts.map((accountIndex: number) => accountKeys[accountIndex]),
+                            };
+                        })
+                    )
+                );
+                return { ...instructionData, inner: inner_ixs };
+            }
+
+            return instructionData;
+        })
+    );
+
+    return { ixs, programs };
+};
