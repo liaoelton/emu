397c29e7f513e5794bb1c3dcc189e00b7bf78fb5
Elton Liao <41299809+liaoelton@users.noreply.github.com>
2024-07-31 21:22:35 +0800

    feat: Add rate limiter to API (#21)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import expressRateLimit from "express-rate-limit";
 import { errorHandler } from "./middlewares/errorHandler";
 import blockRoutes from "./routes/blockRoutes";
 import searchRoutes from "./routes/searchRoutes";
@@ -10,9 +11,16 @@ import { connect_to_db } from "./utils/db";
 
 const app = express();
 const port = 3001;
+const apiLimiter = expressRateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+});
 
 app.use(express.json());
 app.use(cors());
+app.use(apiLimiter);
 
 app.use("/slot", slotRoutes);
 app.use("/blocks", blockRoutes);
