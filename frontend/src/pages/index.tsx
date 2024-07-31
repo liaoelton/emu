f84eb2cbc73cd52058f7674c69b9bb3314f6f2a3
Elton Liao <41299809+liaoelton@users.noreply.github.com>
2024-08-01 00:19:20 +0800

    fix: Align recent block start slot with current slot and improve sorting (#23)
    - set start slot of recent blocks to match initial current slot
    - improve sorting to compare using slot when keys are identical

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,21 @@
 import CurrentBlockStatus from "@/components/CurrentBlockStatus";
 import RecentBlockList from "@/components/RecentBlockList";
+import { useSlot } from "@/hooks/useSlot";
+import { useState } from "react";
 
 export default function Home() {
+    const { slot: currentSlot, loading, error } = useSlot();
+    const [recentSlot, setRecentSlot] = useState<number | null>(null);
+    const updateRecentSlot = () => {
+        setRecentSlot(currentSlot);
+    };
+    if (!recentSlot && currentSlot) {
+        setRecentSlot(currentSlot);
+    }
     return (
         <>
-            <CurrentBlockStatus />
-            <RecentBlockList />
+            <CurrentBlockStatus slot={currentSlot} />
+            <RecentBlockList startSlot={recentSlot} renewStartSlot={updateRecentSlot} />
         </>
     );
 }
